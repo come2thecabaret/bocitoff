a9d6be5636827401d6a8b6c6a50f7ace274dbb51
Joey Conroy <come2thecabaret@gmail.com>
2016-07-20 15:31:39 -0400

    item partials working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,20 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+      @item = current_user.items.build(item_params)
+      if @item.save
+          redirect_to user_path(current_user)
+      else
+        flash[:error] = "Error adding item. Please try again."
+        redirect_to user_path(current_user)
+      end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'items/new'
 
   devise_for :users, controllers: { registrations: 'registrations' }
 
