279f39e1cf9698159fecaf84017ec114a99736cc
Joey Conroy <come2thecabaret@gmail.com>
2016-07-20 13:20:36 -0400

    changed route for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'welcome/about'
 
+
+  authenticated :user do
+      root to: 'users#show', as: :authenticated_root
+  end
   root 'welcome#index'
 end
