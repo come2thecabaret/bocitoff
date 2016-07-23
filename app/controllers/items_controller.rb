6f9c327caba7b04808171aee4387015a3758e87c
Joey Conroy <come2thecabaret@gmail.com>
2016-07-22 23:51:41 -0400

    delete working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,9 +13,23 @@ class ItemsController < ApplicationController
       end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+        flash[:notice] = "Item removed from To-do list. Way to go!"
+        redirect_to authenticated_root_path
+    else
+      flash[:error] = "Error adding item. Please try again."
+      redirect_to authenticated_root_path
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name)
   end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :users, only: [:show] do
-   resources :items, only: [:create]
+   resources :items, only: [:create, :destroy]
 end
 
   get 'welcome/about'
